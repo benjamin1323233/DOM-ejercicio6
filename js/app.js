2818a12fd6837d835459f0bd481da3035bae60de
Quiros Benjamin <bq170921@gmail.com>
2025-05-21 16:03:21 -0300

    realice el final del ejercicio 6

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+let horas = 0;
+let minutos = 0;
+let segundos = 1;
+let idsetInterval = null
+    function reanudar () {
+        if (idsetInterval === null) {
+          idsetInterval = setInterval(temporizador, 1000);
+        }
+        }
 function iniciar(e) {
   e.preventDefault();
   divTemporizador.classList.remove("d-none");
@@ -5,50 +14,70 @@ function iniciar(e) {
   btnreiniciar.classList.remove("d-none");
   btniniciar.classList.remove("d-none");
   formulario.classList.add("d-none");
-  let horas = parseInt(document.getElementById("horas").value);
-  let minutos = parseInt(document.getElementById("minutos").value);
-  let segundos = parseInt(document.getElementById("segundos").value);
-
-  function temporizador() {
-   if(horas==0&&minutos==0&&segundos==0){
-    horas="00"
-    minutos="00"
-    segundos="00"
-   }else{
-    if(segundos==0){
-        minutos--
-        segundos=59
-    }
-    else{
-        segundos--
-    }
-    if(minutos==0&&segundos==0){
-        horas--
-        minutos=59
-    }
+   horas = parseInt(document.getElementById("horas").value);
+   minutos = parseInt(document.getElementById("minutos").value);
+   segundos += parseInt(document.getElementById("segundos").value);
+   temporizador()
+   console.log(e)
    }
-    
+
+function temporizador() {
+    if (horas == 0 && minutos == 0 && segundos == 0) {
+      horas = "00";
+      minutos = "00";
+      segundos = "00";
+    } else {
+      if (segundos == 0) {
+        minutos--;
+        segundos = 59;
+      } else {
+        segundos--;
+      }
+      if (minutos == 0 && segundos == 0) {
+        horas--;
+        minutos = 59;
+      }
+    }
+  
     parrafoTemporizador.textContent = `${horas}:${minutos}:${segundos}`;
-    if(horas<10){
-        parrafoTemporizador.textContent =`0${horas}:`
-    }else{
-         parrafoTemporizador.textContent =`${horas}:`
+    if (horas < 10) {
+      parrafoTemporizador.textContent = `0${horas}:`;
+    } else {
+      parrafoTemporizador.textContent = `${horas}:`;
     }
-    if(minutos<10){
-        parrafoTemporizador.textContent +=`0${minutos}:`
-    }else{
-         parrafoTemporizador.textContent +=`${minutos}:`
+    if (minutos < 10) {
+      parrafoTemporizador.textContent += `0${minutos}:`;
+    } else {
+      parrafoTemporizador.textContent += `${minutos}:`;
     }
-    if(segundos<10){
-        parrafoTemporizador.textContent +=`0${segundos}:`
-    }else{
-         parrafoTemporizador.textContent +=`${segundos}:`
+    if (segundos < 10) {
+      parrafoTemporizador.textContent += `0${segundos}`;
+    } else {
+      parrafoTemporizador.textContent += `${segundos}`;
     }
     console.log(horas, minutos, segundos);
   }
+  
 
-  setInterval(temporizador, 1000);
+
+function reiniciar (){
+    clearInterval(idsetInterval)
+    divTemporizador.classList.add("d-none");
+    btnpausar.classList.add("d-none");
+    btnreiniciar.classList.add("d-none");
+    btniniciar.classList.add("d-none");
+    formulario.classList.remove("d-none");
+    horas = 0
+    minutos = 0
+    segundos = 0
+
 }
+function pausar () {
+    clearInterval(idsetInterval);
+    idsetInterval = null; 
+  }
+
+
 const parrafoTemporizador = document.getElementById("temporizador");
 const divTemporizador = document.getElementById("divtemporizador");
 
@@ -58,3 +87,9 @@ const btnpausar = document.getElementById("pausar");
 const btnreiniciar = document.getElementById("reiniciar");
 
 formulario.addEventListener("submit", iniciar);
+
+btnreiniciar.addEventListener("click",reiniciar)
+
+btnpausar.addEventListener("click",pausar)
+
+btniniciar.addEventListener("click",reanudar)
